c8ec0cd776aaa7c055d682cf39ce5d80ed89a80d
Tomaszius <liutkus.to@gmail.com>
2022-06-27 13:05:35 +0300

    kažkokia paleva

--- src/Programa.java
+++ src/Programa.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Programa {
     public static void main(String[] args) {
+        Scanner skaitytuvas = new Scanner(System.in);
+        int w = skaitytuvas.nextInt();
 
+        System.out.println(konvertuojaYesirNo(arSkaiciusLyginis(w)));
+
+    }
+
+    public static boolean arSkaiciusLyginis(int x){
+        return x % 2 == 0 && x > 2;
+    }
+    
+    public static String konvertuojaYesirNo(boolean b){
+        if (b){
+            return "YES";
+        }
+        else{
+            return "NO";
+        }
     }
 }
